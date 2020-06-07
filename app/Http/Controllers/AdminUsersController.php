17759039febd04cbdaf46ab7306d48974000be45
ahmad <hinafraz025@gmail.com>
2020-06-07 23:36:57 +0500

    installed new git, and added photos table

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
+use Illuminate\Http\Request;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = User::all();
+        return view('admin.users.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //used for showing form to create a user
+        //passed role name and id as an array to view
+        $roles = Role::pluck('name','id')->all();
+        return view('admin.users.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    //created my manual UsersRequest for validation purposes
+    public function store(UsersRequest $request)
+    {
+        //gets form data and save it,
+        User::create($request->all());
+        //and then goes to show all users
+        return redirect('/admin/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+@section('content')
+    <h1>Create Admin Users</h1>
+{{--files=>true is equivalent to enctype=multipart/form-data --}}
+            {!! Form::open(['method'=>'POST', 'action' => 'AdminUsersController@store','files'=>true]) !!}
+    <div class="form-group">
+            {!!   Form::label('name','Name', ['class'=>'offset-4']) !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+            </div>
+    <div class="form-group">
+            {!!   Form::label('email','Email', ['class'=>'offset-4']) !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            </div>
+    <div class="form-group">
+            {!!   Form::label('role_id','Role', ['class'=>'offset-4']) !!}
+            {!! Form::select('role_id',[''=>'Choose Options'] + $roles ,null, ['class'=>'form-control']) !!}
+            </div>
+    <div class="form-group">
+            {!!   Form::label('is_active','Status', ['class'=>'offset-4']) !!}
+            {!!   Form::select('is_active',array(1=>'Active',0=>'Not Active') ,0, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+                {!!   Form::label('file','Name', ['class'=>'offset-4']) !!}
+                {!! Form::file('file', null, ['class'=>'form-control']) !!}
+                </div>
+             {{csrf_field()}}
+    <div class="form-group">
+        {!!   Form::label('password','Password') !!}
+        {!!   Form::password('password',['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+            {!!    Form::submit('Create Admin User', ['class'=>'btn btn-primary btn-block']) !!}
+               {!! Form::close() !!}
+        </div>
+    @include('includes.form_errors')
+    @endsection
